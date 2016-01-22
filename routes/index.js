5458fc98e4451d98d6ed0d370bac83bd2230e575
Jayadeep KM <kmjayadeep@gmail.com>
2016-01-22 22:28:33 +0530

    complete

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.json({
+        ipAddress: req.connection.remoteAddress,
+        language: req.headers['accept-language'].split(',')[0],
+        software: req.headers['user-agent'].split('(')[1].split(')')[0]
+    })
+});
+
+module.exports = router;
