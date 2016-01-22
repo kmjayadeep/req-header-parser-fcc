90d6e09ad10cc4d9bcde4ed324ad2484c4ecaf3e
Jayadeep KM <kmjayadeep@gmail.com>
2016-01-22 22:45:19 +0530

    minor fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.json({
-        ipAddress: req.connection.remoteAddress,
+        ipAddress: req.headers['x-forwarded-for'],
         language: req.headers['accept-language'].split(',')[0],
         software: req.headers['user-agent'].split('(')[1].split(')')[0]
     })
